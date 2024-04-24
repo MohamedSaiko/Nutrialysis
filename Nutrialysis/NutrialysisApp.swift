d70867c2571956ba26975e52adf5bfc3d9851e96
MohamedSayed <Mohamedsaiko9@gmail.com>
2024-04-24 11:07:05 +0200

    Initial Commit

---
+++ Nutrialysis/NutrialysisApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutrialysisApp.swift
+//  Nutrialysis
+//
+//  Created by Mohamed Sayed on 24.04.24.
+//
+
+import SwiftUI
+
+@main
+struct NutrialysisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
