f88131f8a1f84c4a1b2a6a534a9d9395c90785a2
MohamedSayed <Mohamedsaiko9@gmail.com>
2024-04-28 11:54:27 +0200

    Update Constants

---
+++ Nutrialysis/Constants/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  Nutrialysis
+//
+//  Created by Mohamed Sayed on 24.04.24.
+//
+
+import Foundation
+
+// MARK: URLs Constants
+
+let searchingURL = "https://trackapi.nutritionix.com/v2/search/instant?query="
+let foodNutrientsURL = "https://trackapi.nutritionix.com/v2/natural/nutrients"
+
+// MARK: CoreData Constants
+
+let foodContainer = "FoodContainer"
+let foodEntity = "FoodEntity"
